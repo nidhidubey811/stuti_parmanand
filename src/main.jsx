8e4707a8a5485997bf75e8fb074a768f05de1021
nidhidubey811 <73709657+nidhidubey811@users.noreply.github.com>
2025-07-28 22:27:58 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import PortfolioPage from './PortfolioPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortfolioPage />
+  </React.StrictMode>,
+)
